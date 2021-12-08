5eb62a85251688a55b0bccf3ea28526dcbb0108b
Joaquim Bolós Fernández <93405477+Quimbolos@users.noreply.github.com>
2021-12-08 10:36:20 +0000

    Add files via upload

---
+++ 3 - Decimal into Binary.py
@@ -0,0 +1,9 @@
+import math
+
+def DecimalToBinary(DecNum):
+
+  print(str("Equivalent binary number: "), bin(DecNum))
+
+DecNum = int(input("Enter any decimal number: "))
+
+DecimalToBinary(DecNum)
