85882395330b9fdd1b49be7a4817e2de5e7ca59c
Joaquim Bolós Fernández <93405477+Quimbolos@users.noreply.github.com>
2021-12-08 10:52:42 +0000

    Delete 3 - Decimal into Binary.py

--- 3 - Decimal into Binary.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def DecimalToBinary(DecNum):
-
-  print(str("Equivalent binary number: "), bin(DecNum))
-
-DecNum = int(input("Enter any decimal number: "))
-
-DecimalToBinary(DecNum)
